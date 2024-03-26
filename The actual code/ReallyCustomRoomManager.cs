ea109b34656f1da6975e827d5956742c66c6df01
Minepool9 <97184060+Minepool9@users.noreply.github.com>
2024-03-26 03:21:52 +0200

    Add files via upload

---
+++ The actual code/ReallyCustomRoomManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace DoomahLevelLoader
+{
+public class ReallyCustomRoomManager : MonoBehaviour
+{
+    // Public list for Active Rooms
+    public List<GameObject> ActiveRooms = new List<GameObject>();
+}
+}
