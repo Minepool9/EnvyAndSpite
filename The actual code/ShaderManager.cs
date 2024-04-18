63fdde69fe80e9acc946056c256e1ae54552733c
Minepool9 <97184060+Minepool9@users.noreply.github.com>
2024-04-18 03:04:08 +0200

    fuck fuck fuck

---
+++ The actual code/ShaderManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using System.Threading.Tasks;
+
+public static class ShaderManager
+{
+    private static Shader unlitShader;
+    private static Shader vertexLitShader;
+    private static Shader transparentShader;
+
+    private static Dictionary<string, Shader> shaderDictionary;
+
+    public static async Task LoadShaders()
+    {
+        AsyncOperationHandle<Shader> unlitHandle = Addressables.LoadAssetAsync<Shader>("Assets/Shaders/Main/ULTRAKILL-unlit.shader");
+        await unlitHandle.Task;
+        unlitShader = unlitHandle.Result;
+
+        AsyncOperationHandle<Shader> vertexLitHandle = Addressables.LoadAssetAsync<Shader>("Assets/Shaders/Main/ULTRAKILL-vertexlit.shader");
+        await vertexLitHandle.Task;
+        vertexLitShader = vertexLitHandle.Result;
+        
+        AsyncOperationHandle<Shader> transparentHandle = Addressables.LoadAssetAsync<Shader>("Assets/Shaders/Transparent/ULTRAKILL-vertexlit-transparent-zwrite.shader");
+        await transparentHandle.Task;
+        transparentShader = transparentHandle.Result;
+
+        shaderDictionary = new Dictionary<string, Shader>
+        {
+            { "ULTRAKILL/VERTEXLIT", vertexLitShader },
+            { "Standard", unlitShader },
+            { "ULTRAKILL/UNLIT", unlitShader },
+            { "ULTRAKILL/TRANSPARENT", transparentShader },
+        };
+    }
+
+    public static void ApplyShaders(GameObject[] allGameObjects)
+    {
+        foreach (GameObject go in allGameObjects)
+        {
+            MeshRenderer[] meshRenderers = go.GetComponentsInChildren<MeshRenderer>(true);
+
+            foreach (MeshRenderer renderer in meshRenderers)
+            {
+                if (renderer.gameObject.name == "Quad")
+                {
+                    continue;
+                }
+
+                if (shaderDictionary.ContainsKey(renderer.material.shader.name))
+                {
+                    Shader shader = shaderDictionary[renderer.material.shader.name];
+                    ApplyShaderToRenderer(renderer, shader);
+                }
+            }
+        }
+    }
+
+    private static void ApplyShaderToRenderer(MeshRenderer renderer, Shader shader)
+    {
+        Material[] materials = renderer.materials;
+        foreach (Material material in materials)
+        {
+            material.shader = shader;
+        }
+        renderer.materials = materials;
+    }
+}
