f169812bc723c7be458702d338e6ce47910f5f35
Minepool9 <minepooljunk@gmail.com>
2024-04-24 18:39:19 +0200

    added clash support

---
+++ The actual code/Editor Components/ClashTriggers.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System;
+
+namespace DoomahLevelLoader.UnityComponents
+{
+	public class ClashTriggerDisable : MonoBehaviour
+	{
+		private void OnTriggerEnter(Collider other)
+		{
+			bool flag = other.gameObject.tag == "Player";
+			if (flag)
+			{
+				MonoSingleton<PlayerTracker>.Instance.ChangeToFPS();
+			}
+		}
+	}
+	
+	public class ClashTriggerEnable : MonoBehaviour
+	{
+		private void OnTriggerEnter(Collider other)
+		{
+			bool flag = other.gameObject.tag == "Player" && !this.hasenabled;
+			if (flag)
+			{
+				MonoSingleton<PlayerTracker>.Instance.ChangeToPlatformer();
+				bool onlyOnce = this.OnlyOnce;
+				if (onlyOnce)
+				{
+					this.hasenabled = true;
+				}
+			}
+		}
+
+		private bool hasenabled = false;
+
+		public bool OnlyOnce = false;
+	}
+}
