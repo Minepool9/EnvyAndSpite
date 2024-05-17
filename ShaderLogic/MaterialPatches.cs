16bf771f22f3e629e3c29525df8d6001fb4b4467
ImNotSimon <129795437+ImNotSimon@users.noreply.github.com>
2024-05-17 18:52:05 +0100

    Update MaterialPatches.cs

--- ShaderLogic/MaterialPatches.cs
+++ ShaderLogic/MaterialPatches.cs
@@ -1,47 +1,46 @@
-﻿using HarmonyLib;
+using HarmonyLib;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using UnityEngine;
 
 namespace DoomahLevelLoader
 {
-	[HarmonyPatch(typeof(Material))]
-	static class MaterialPatches
-	{
-		public static void Process(Material material)
-		{
-			if (material.shader == null)
-				return;
+    [HarmonyPatch(typeof(Material))]
+    static class MaterialPatches
+    {
+        public static void Process(Material material)
+        {
+            if (material.shader == null)
+                return;
 
-			if (!ShaderManager.shaderDictionary.TryGetValue(material.shader.name, out Shader realShader))
-				return;
+            if (!ShaderManager.shaderDictionary.TryGetValue(material.shader.name, out Shader realShader))
+                return;
 
-			if (material.shader == realShader)
-				return;
+            if (material.shader == realShader)
+                return;
 
-			material.shader = realShader;
-		}
+            material.shader = realShader;
+        }
 
-		[HarmonyPatch(MethodType.Constructor, new Type[] {typeof(Shader)})]
-		[HarmonyPostfix]
-		public static void CtorPatch1(Material __instance)
-		{
-			Process(__instance);
-		}
+        [HarmonyPatch(MethodType.Constructor, new Type[] { typeof(Shader) })]
+        [HarmonyPostfix]
+        public static void CtorPatch1(Material __instance)
+        {
+            Process(__instance);
+        }
 
-		[HarmonyPatch(MethodType.Constructor, new Type[] { typeof(Material) })]
-		[HarmonyPostfix]
-		public static void CtorPatch2(Material __instance)
-		{
-			Process(__instance);
-		}
+        [HarmonyPatch(MethodType.Constructor, new Type[] { typeof(Material) })]
+        [HarmonyPostfix]
+        public static void CtorPatch2(Material __instance)
+        {
+            Process(__instance);
+        }
 
-		[HarmonyPatch(MethodType.Constructor, new Type[] { typeof(string) })]
-		[HarmonyPostfix]
-		public static void CtorPatch3(Material __instance)
-		{
-			Process(__instance);
-		}
-	}
+        [HarmonyPatch(MethodType.Constructor, new Type[] { typeof(string) })]
+        [HarmonyPostfix]
+        public static void CtorPatch3(Material __instance)
+        {
+            Process(__instance);
+        }
+    }
 }
